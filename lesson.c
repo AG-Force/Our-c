a7c0a52c9cb3c3299e574d769caa17bca132e53c
AG-Force <randa712807@icloud.com>
2023-06-09 12:59:13 +0200

    first app

--- lesson.c
+++ lesson.c
@@ -1,9 +1,20 @@
-#include<stdio.h>
+#include<stdio.h> // the library
 int main ()
-{
+
+
+
+
+
+               {
 printf("Hallo Welt\n");
-return 0;
+
+return 0; // The running of the following program was successful; it can be quit. It means the programm will automatically closed after running
+
+// getchar(); means the program will be quit after pressing any key
 
 
 
-}
+
+
+
+                }
