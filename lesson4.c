f74bb14577fc3575c676974164d11ca810595c45
Gassan Abdalhamid <gassan@localhost.localdomain>
2023-06-17 00:07:12 +0200

    variable char

--- lesson4.c
+++ lesson4.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main ()
+int main()
 {
-char name [30];
-printf("enter the name: ");
-scanf("%s", name);
-printf("the name: %s", name);
+    char name[30]; // Declare a character array named "name" with a size of 30 characters.
 
+    printf("enter the name: "); // Print a prompt to ask the user to enter a name.
+    scanf("%s", name); // Read the entered name and store it in the variable "name".
+
+    printf("the name: %s", name); // Print the stored name on the screen.
 
     return 0;
 }
