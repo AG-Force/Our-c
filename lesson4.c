3dc1e3fcd3dd92cbdf708f4323f211c2d2b7316c
Gassan Abdalhamid <gassan@localhost.localdomain>
2023-06-19 14:45:52 +0200

    char string

--- lesson4.c
+++ lesson4.c
@@ -5,7 +5,7 @@ int main()
     char name[30]; // Declare a character array named "name" with a size of 30 characters.
 
     printf("enter the name: "); // Print a prompt to ask the user to enter a name.
-    scanf("%s", name); // Read the entered name and store it in the variable "name".
+    scanf("%s", name); // Read the entered name and store it in the variable "name". Bei strings ist & nicht notwendig
 
     printf("the name: %s", name); // Print the stored name on the screen.
 
