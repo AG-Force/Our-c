0f76a33ba51f3599d1599b959fc0aaf9bf184695
AG <85740749+AG-Force@users.noreply.github.com>
2023-06-29 08:54:15 +0200

    Add files via upload

---
+++ lesson14.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+/*
+    int N;
+    printf("Enter the number: ");
+    scanf("%d", &N);
+    
+    int s = 0;
+    for (int i = 1; i <= N; i++) {
+        s = s + i;
+    }
+    
+    printf("Sum: %d\n", s);
+
+
+ */
+//###################################################################################//
+/*
+ int N;
+    printf("Enter the number: ");
+    scanf("%d", &N);
+    
+    int s = 0;
+    for (int i = 5; i > 0; i--) {
+        s = s + i;
+    }
+    
+    printf("Sum: %d\n", s);
+
+
+*/
+ //###################################################################################//
+/*
+
+
+int N;
+    printf("Enter the number: ");
+    scanf("%d", &N);
+    
+    int s = 0; int i =1;
+     while (i<=N)
+     {
+        s= s+ i;
+        i++;
+     }
+     
+    
+    printf("Sum: %d\n", s);
+
+
+*/
+ //###################################################################################//
+/*
+
+nt N;
+    printf("Enter the number: ");
+    scanf("%d", &N);
+    
+    int s = 0; int i =N;
+     while (i>0)
+     {
+        s= s+ i;
+        i--;
+     }
+     
+    
+    printf("Sum: %d\n", s);
+
+
+*/
+ //###################################################################################//
+ /*
+ 
+ int N;
+    printf("Enter the number: ");
+    scanf("%d", &N);
+    
+    int s = 0; int i =1;
+    do{
+        s = s + i;
+        i++;
+    }
+     while (i<N+1);
+    
+    printf("Sum: %d\n", s);
+ 
+ */
+ //###################################################################################//
+
+int N;
+    printf("Enter the number: ");
+    scanf("%d", &N);
+    
+    int s = 0; int i =1;
+    do{
+        s = s + i;
+        i++;
+    }
+     while (i<=N);
+    
+    printf("Sum: %d\n", s);
+ //###################################################################################//
+
+/*
+ int N;
+    printf("Enter the number: ");
+    scanf("%d", &N);
+    
+    int s = 0; int i =N;
+    do{
+        s = s + i;
+        i--;
+    }
+     while (i>0);
+    
+    printf("Sum: %d\n", s);
+ 
+*/
+    return 0;
+
+}
